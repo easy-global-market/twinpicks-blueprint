b14aa08102651e57aab4a0089bb9917003213d55
Romain Magnani <romainm.web@gmail.com>
2024-07-22 13:34:26 +0200

    fix (QAI): remove device and add airQualityObserved (#32)
    Co-authored-by: Luc Gasser <gasserluc@gmail.com>

---
+++ src/usecases/air-quality-monitoring/airQualityObserved.ts
@@ -0,0 +1,51 @@
+import { StellioTemplate } from 'src/interfaces';
+
+export const AirQualityObservedTemplate: StellioTemplate = {
+    id: 'urn:ngsi-ld:AirQualityObserved:Template',
+    type: 'Template',
+    name: {
+        type: 'Property',
+        value: 'Placeholder',
+        jsonSchema: {
+            type: 'Property',
+            value: {
+                schemaType: 'string',
+                title: 'Nom de l\'entité',
+            },
+        },
+    },
+    description: {
+        type: 'Property',
+        value: 'Placeholder',
+        jsonSchema: {
+            type: 'Property',
+            value: {
+                schemaType: 'string',
+                title: 'Description de l\'entité',
+            },
+        },
+    },
+    location: {
+        type: 'GeoProperty',
+        value: {
+            type: 'Point',
+            coordinates: [],
+        },
+        jsonSchema: {
+            type: 'Property',
+            value: {
+                schemaType: 'object',
+                title: 'Où se situe les mesures',
+            },
+        },
+    },
+    jsonSchema: {
+        type: 'Property',
+        value: {
+            schemaType: 'AirQualityObserved',
+            title: 'Qualité de l\'air extérieur',
+            required: ['name', 'location'],
+            description: "Jumeau numérique de l\'observation de la qualité de l'air extérieur",
+        },
+    },
+};
