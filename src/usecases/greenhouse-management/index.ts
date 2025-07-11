9efbd81ff5c13ae804d91df1d3a8b405e9eaea53
junjou <romainm.web@gmail.com>
2025-07-11 17:03:18 +0200

    fix: add RefSoilArea

--- src/usecases/greenhouse-management/index.ts
+++ src/usecases/greenhouse-management/index.ts
@@ -1,9 +1,10 @@
 import Building from './building';
 import GreenouseTable from './greenhouseTable';
+import ReferenceSoilArea from './referenceSoilArea';
 
 import { generateBlueprintFile } from '../../utils';
 
-const blueprint = [Building, GreenouseTable];
+const blueprint = [Building, GreenouseTable, ReferenceSoilArea];
 
 const usecasePathName = __dirname.split(/dist[\\/]/)[1];
 
